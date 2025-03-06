b5ae12c591aee31b383f68d607e5cda27366bfaa
asianlink-lloyd <johnlloydbutay123@gmail.com>
2025-03-06 16:36:41 +0800

    Rows and Columns

--- lib/main.dart
+++ lib/main.dart
@@ -15,27 +15,36 @@ class MyWidget extends StatelessWidget {
         backgroundColor: Colors.purple,
         titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(10),
-          alignment: Alignment.center,
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            color: Colors.purple,
-            borderRadius: BorderRadius.circular(10),
-            gradient: LinearGradient(colors: [Colors.purple, Colors.pink]),
-            boxShadow: [BoxShadow(color: Colors.purple, blurRadius: 5)],
-          ),
-          child: Text(
-            "I am a box",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+      body: Container(
+        color: Colors.purple,
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
             ),
-          ),
+            Container(
+              padding: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+          ],
         ),
       ),
     );
