ff69590f9b68d29887acc178c5fb8cd5e4c239be
asianlink-lloyd <johnlloydbutay123@gmail.com>
2025-03-07 11:33:05 +0800

    ch6 completed
    Netwoking with HTTP,
    ListView Builder,
    GridView Builder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/drawer.dart';
-import 'package:flutter_application_2/name_card_widget.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,13 +12,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var myText = "Change my Name";
-  TextEditingController _nameController = TextEditingController();
+  // var myText = "Change my Name";
+  // TextEditingController _nameController = TextEditingController();
+
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(Uri.parse(url));
+    data = jsonDecode(res.body);
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
   }
 
   @override
@@ -27,22 +45,40 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.purple,
         titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: NameCardWidget(
-              myText: myText,
-              nameController: _nameController,
-            ),
-          ),
-        ),
-      ),
+      body:
+          data == null
+              ? const Center(
+                child: CircularProgressIndicator(),
+              ) // Show loading indicator
+              : ListView.builder(
+                itemCount:
+                    data.length, // Access `length` only when `data` is not null
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(data[index]["title"]),
+                    subtitle: Text("ID: ${data[index]["id"]}"),
+                    leading: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        // Rounded corners // Optional border
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://images.unsplash.com/photo-1511367461989-f85a21fda167?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWFuJTIwaWNvbnxlbnwwfHwwfHx8MA%3D%3D",
+                          ),
+                          fit: BoxFit.cover, // Proper scaling
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          myText = _nameController.text;
-          setState(() {});
+          // myText = _nameController.text;
+          // setState(() {});
         },
         backgroundColor: Colors.purple,
         child: Icon(Icons.send, color: Colors.white),
