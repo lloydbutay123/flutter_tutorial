0af316a41cab8ff558a5b9e37817354fa848d240
asianlink-lloyd <johnlloydbutay123@gmail.com>
2025-03-07 09:24:58 +0800

    Updated ch5
    Card,
    Asset Images,
    TextField, ScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyWidget()));
+  runApp(
+    MaterialApp(theme: ThemeData(useMaterial3: true), home: const HomePage()),
+  );
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myText = "Change my Name";
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +32,37 @@ class MyWidget extends StatelessWidget {
         titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
       ),
       body: Center(
-        child: Container(color: Colors.yellow, height: 100, width: 100),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.network(
+                    "https://images.unsplash.com/photo-1639506523002-83d6fbfb72fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNvbXB1dGVyJTIwd2l0aCUyMHB1cnBsZXxlbnwwfHwwfHx8MA%3D%3D",
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    myText,
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter your name",
+                        labelText: "Name",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
       drawer: Drawer(
         child: ListView(
@@ -49,9 +95,12 @@ class MyWidget extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
         backgroundColor: Colors.purple,
-        child: Icon(Icons.edit, color: Colors.white),
+        child: Icon(Icons.send, color: Colors.white),
       ),
     );
   }
